32ac1efadbcea4f46fcc1b97679f2df1d133d675
zeeshan-za-ahmad <zeeshan.za.ahmad@gmail.com>
2017-09-08 01:52:40 +0500

    Add support for text formate parsing

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,6 +1,24 @@
 class ConverterController < ApplicationController
+
+  before_action :validates_format, only: [:import]
+
+  def validates_format
+    
+    accepted_formats = [".txt", ".csv"]
+
+    if params[:file] and !accepted_formats.include? File.extname(params[:file].original_filename)
+      flash[:error] = "This file formate is not allowed!"
+      render :template => "converter/index"
+    end
+
+  end
+
   def import
-    @quize = Converter.import(params[:file])
+
+    @quize = Converter.importXml(params[:file]) if [".csv"].include? File.extname(params[:file].original_filename)
+    
+    @quize = Converter.importText(params[:file]) if [".txt"].include? File.extname(params[:file].original_filename)
+
     render :template => "converter/index.xml", :type => :builder
   end
 end
--- app/models/converter.rb
+++ app/models/converter.rb
@@ -1,7 +1,7 @@
 class Converter < ApplicationRecord
   require 'csv'
 
-  def self.import(file)
+  def self.importXml(file)
       quiz = []
       answers = []
       questions = []
@@ -53,4 +53,55 @@ class Converter < ApplicationRecord
     quiz.push questions
   end
   
+  def self.importText(file)
+      quiz = []
+      answers = []
+      questions = []
+      question = nil
+    
+      is_question = true
+      can_parse_answers = false
+      
+    File.open(file.path, "r").each_line do |line|
+      
+      line = line.squish
+ 
+      if is_question && (line[-1,1] == "?")
+        question  = line
+        is_question = false
+        # now turn for answers parsing
+        can_parse_answers = true
+      elsif line.downcase.include? "answer:"
+        data = line.split(":")
+
+        temp_answer = data[1].squish
+
+        tempa = {question: question , answers: answers, answer: temp_answer}
+
+        questions.push tempa
+
+        is_question = true
+ 
+        answers.each do |answer|
+          if answer[:option] == temp_answer
+            answer[:option] = temp_answer
+          else
+            answer[:option] = 0
+          end 
+        end
+        # empty the answers 
+        answers = []
+        is_question = true
+        can_parse_answers = false
+
+      elsif !is_question && can_parse_answers
+          option = line[0..1].chomp(".")
+          text = line[2,line.length].squish
+          temp = {option: option , text: text}
+          answers.push temp
+      end
+      puts line
+    end
+    quiz.push questions
+  end
 end
